c5b9ae3464feb1bad1f33fd4e420c9b75da4cb85
unknown <bsa6.23.94@gmail.com>
2018-09-24 16:39:18 -0400

    added some boilerplate to the backend

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json());
+
+module.exports = server;
